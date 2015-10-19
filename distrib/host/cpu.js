87f58e82e520151fc3936a969c6251897d49a039
Michael Pair <michael.pair1@marist.edu>
2015-10-19 11:44:19 -0400

    progress on the CPU
    the CPU is slowly working more and more, but its not fully functional
    yet

--- distrib/host/cpu.js
+++ distrib/host/cpu.js
@@ -16,18 +16,20 @@
 var TSOS;
 (function (TSOS) {
     var Cpu = (function () {
-        function Cpu(PC, Acc, Xreg, Yreg, Zflag, isExecuting) {
+        function Cpu(PC, Acc, Xreg, Yreg, Zflag, Instruction, isExecuting) {
             if (PC === void 0) { PC = 0; }
             if (Acc === void 0) { Acc = 0; }
             if (Xreg === void 0) { Xreg = 0; }
             if (Yreg === void 0) { Yreg = 0; }
             if (Zflag === void 0) { Zflag = 0; }
+            if (Instruction === void 0) { Instruction = ""; }
             if (isExecuting === void 0) { isExecuting = false; }
             this.PC = PC;
             this.Acc = Acc;
             this.Xreg = Xreg;
             this.Yreg = Yreg;
             this.Zflag = Zflag;
+            this.Instruction = Instruction;
             this.isExecuting = isExecuting;
         }
         Cpu.prototype.init = function () {
@@ -42,20 +44,18 @@ var TSOS;
             _Kernel.krnTrace('CPU cycle');
             // TODO: Accumulate CPU usage and profiling statistics here.
             // Do the real work here. Be sure to set this.isExecuting appropriately.
-            this.execute(this.grabMemory());
+            this.execute(memoryManager.getMemory(this.PC));
             this.updateCPU();
         };
-        Cpu.prototype.grabMemory = function () {
-            return memoryManager.getMemory(this.PC);
-        };
         Cpu.prototype.updateCPU = function () {
-            //if (this.isExecuting) {s
-            programs[_ExecutingProgram].PC = this.PC;
-            programs[_ExecutingProgram].Acc = this.Acc;
-            programs[_ExecutingProgram].Xreg = this.Xreg;
-            programs[_ExecutingProgram].Yreg = this.Yreg;
-            programs[_ExecutingProgram].Zflag = this.Zflag;
-            //}
+            if (this.isExecuting) {
+                programs[_ExecutingProgram].PC = this.PC;
+                programs[_ExecutingProgram].Instruction = this.Instruction;
+                programs[_ExecutingProgram].Acc = this.Acc;
+                programs[_ExecutingProgram].Xreg = this.Xreg;
+                programs[_ExecutingProgram].Yreg = this.Yreg;
+                programs[_ExecutingProgram].Zflag = this.Zflag;
+            }
         };
         /*public runProgram() {
             this.cycle()
@@ -68,91 +68,110 @@ var TSOS;
 
         }*/
         Cpu.prototype.execute = function (instructions) {
-            if (instructions = "A9") {
-                //Loads the accumulator with a constant
-                this.Acc = memoryManager.convertHex(memoryManager.getMemory(++this.PC));
-                assemblerCode = "LDA #$" + memoryManager.getMemory(this.PC);
-            }
-            else if (instructions = "AD") {
-                //loads the accumulator from memory
-                this.Acc = memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC));
-                assemblerCode = "LDA $0" + memoryManager.getMemory(this.PC);
-                this.PC++;
-            }
-            else if (instructions = "6D") {
-                //Adds with Carry: Adds contents of an address to the contents
-                // of the accumulator and keeps the result in the accumulator
-                memoryManager.storeInMemory(++this.PC, this.Acc);
-                assemblerCode = "ADC $" + memoryManager.getMemory(this.PC);
-                this.PC++;
-            }
-            else if (instructions = "A2") {
-                //Loads the X register with a constant
-                this.Xreg = memoryManager.convertHex(memoryManager.getMemory(++this.PC));
-                assemblerCode = "LDX #$" + memoryManager.getMemory(this.PC);
-                this.PC++;
-            }
-            else if (instructions = "AE") {
-                //Loads the X register from memory
-                this.Xreg = memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC));
-                assemblerCode = "LDX $" + memoryManager.getMemory(this.PC);
-                this.PC++;
-            }
-            else if (instructions = "A0") {
-                //Loads the Y register with a constant
-                this.Yreg = memoryManager.convertHex(memoryManager.getMemory(++this.PC));
-                assemblerCode = "LDY #$" + memoryManager.getMemory(this.PC);
-                this.PC++;
-            }
-            else if (instructions = "AC") {
-                //Loads the Y register from memory
-                this.Yreg = memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC));
-                assemblerCode = "LDY $" + memoryManager.getMemory(this.PC);
-                this.PC++;
-            }
-            else if (instructions = "EA") {
-                //No operation
-                assemblerCode = "NOP";
-            }
-            else if (instructions = "00") {
-                //Break
-                assemblerCode = "BRK";
-                _KernelInterruptQueue.enqueue(new TSOS.Interrupt(CPU_BRK_IRQ, 2));
-            }
-            else if (instructions = "EC") {
-                //Compares a byte in memory to the the X register
-                //Sets the Z (zero) flag if equal
-                if ((memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC)) == this.Xreg)) {
-                    this.Zflag = 1;
+            this.Instruction = instructions.toUpperCase();
+            switch (this.Instruction) {
+                case "A9": {
+                    //Loads the accumulator with a constant
+                    this.Acc = memoryManager.convertHex(memoryManager.getMemory(++this.PC));
+                    assemblerCode = "LDA #$" + memoryManager.getMemory(this.PC);
+                    break;
                 }
-                else {
-                    this.Zflag = 0;
+                case "AD": {
+                    //loads the accumulator from memory
+                    this.Acc = memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC));
+                    assemblerCode = "LDA $" + memoryManager.getMemory(this.PC);
+                    this.PC++;
+                    break;
                 }
-                assemblerCode = "CPX $" + memoryManager.getNext2Bytes(this.PC);
-                this.PC++;
-            }
-            else if (instructions = "D0") {
-                //Branch N bytes if Z flag = 0
-                if (this.Zflag == 0) {
-                    assemblerCode = "BNE $" + memoryManager.getMemory(this.PC + 1);
-                    this.PC += memoryManager.convertHex(memoryManager.getMemory(++this.PC)) + 1;
+                case "6D": {
+                    //Adds with Carry: Adds contents of an address to the contents
+                    // of the accumulator and keeps the result in the accumulator
+                    memoryManager.storeInMemory(++this.PC, this.Acc);
+                    assemblerCode = "ADC $" + memoryManager.getMemory(this.PC);
+                    this.PC++;
+                    break;
                 }
-                else {
-                    assemblerCode = "BNE $" + memoryManager.getMemory(this.PC);
+                case "A2": {
+                    //Loads the X register with a constant
+                    this.Xreg = memoryManager.convertHex(memoryManager.getMemory(++this.PC));
+                    assemblerCode = "LDX #$" + memoryManager.getMemory(this.PC);
+                    this.PC++;
+                    break;
                 }
-                this.PC++;
+                case "AE": {
+                    //Loads the X register from memory
+                    this.Xreg = memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC));
+                    assemblerCode = "LDX $" + memoryManager.getMemory(this.PC);
+                    this.PC++;
+                    break;
+                }
+                case "A0": {
+                    //Loads the Y register with a constant
+                    this.Yreg = memoryManager.convertHex(memoryManager.getMemory(++this.PC));
+                    assemblerCode = "LDY #$" + memoryManager.getMemory(this.PC);
+                    this.PC++;
+                    break;
+                }
+                case "AC": {
+                    //Loads the Y register from memory
+                    this.Yreg = memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC));
+                    assemblerCode = "LDY $" + memoryManager.getMemory(this.PC);
+                    this.PC++;
+                    break;
+                }
+                case "EA": {
+                    //No operation
+                    assemblerCode = "NOP";
+                    break;
+                }
+                case "00": {
+                    //Break
+                    this.updateCPU();
+                    alert("00");
+                    assemblerCode = "BRK";
+                    _KernelInterruptQueue.enqueue(new TSOS.Interrupt(CPU_BRK_IRQ, 2));
+                    break;
+                }
+                case "EC": {
+                    //Compares a byte in memory to the the X register
+                    //Sets the Z (zero) flag if equal
+                    if ((memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC)) == this.Xreg)) {
+                        this.Zflag = 1;
+                    }
+                    else {
+                        this.Zflag = 0;
+                    }
+                    assemblerCode = "CPX $" + memoryManager.getNext2Bytes(this.PC);
+                    this.PC++;
+                    break;
+                }
+                case "D0": {
+                    //Branch N bytes if Z flag = 0
+                    if (this.Zflag == 0) {
+                        assemblerCode = "BNE $" + memoryManager.getMemory(this.PC + 1);
+                        this.PC += memoryManager.convertHex(memoryManager.getMemory(++this.PC)) + 1;
+                    }
+                    else {
+                        assemblerCode = "BNE $" + memoryManager.getMemory(this.PC);
+                    }
+                    this.PC++;
+                    break;
+                }
+                case "EE": {
+                    //Increment the value of a byte
+                    memoryManager.storeInMemory(this.PC + 1, memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC)));
+                    assemblerCode = "INC" + memoryManager.getNext2Bytes(++this.PC);
+                    this.PC++;
+                    break;
+                }
+                case "FF": {
+                    //System Call
+                    assemblerCode = "SYS";
+                    _KernelInterruptQueue.enqueue(new TSOS.Interrupt(CPU_SYS_IRQ, 3));
+                    break;
+                }
             }
-            else if (instructions = "EE") {
-                //Increment the value of a byte
-                memoryManager.storeInMemory(this.PC + 1, memoryManager.convertHex(memoryManager.getNext2Bytes(++this.PC)));
-                assemblerCode = "INC" + memoryManager.getNext2Bytes(++this.PC);
-                this.PC++;
-            }
-            else if (instructions = "FF") {
-                //System Call
-                assemblerCode = "SYS";
-                _KernelInterruptQueue.enqueue(new TSOS.Interrupt(CPU_SYS_IRQ, 3));
-            }
+            this.PC++;
         };
         return Cpu;
     })();
